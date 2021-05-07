5e69bd34d7a968fc626677dc804bf6836098b8ca
Gorgi <gorgimitev@gmail.com>
2021-05-07 11:42:10 +0200

    Update an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,16 @@
 class ArticlesController < ApplicationController
   def index
-        @articles = Article.all
+    @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
 
@@ -18,8 +21,23 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
     end
 end
+
